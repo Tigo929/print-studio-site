ca02c0b2b9ba83f5c0b9e1c7353f066548365eef
Gulyan Tigran <gulian2014@rambler.ru>
2026-01-06 20:15:53 +0300

    feat: finish header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -9,33 +9,35 @@ export default function Header() {
   const urlParam = usePathname();
   return (
     <header>
-      <div>
-        <Link href={"/"}>
-          <Image src={Logo} alt="Логотип" width={34} height={70} />
-        </Link>
-        <div>
-          <p>PRINT STUDIO</p>
-          <span>Москва • «тонкие армянские ноты»</span>
+      <div className="container mx-auto flex h-18.25 items-center justify-between gap-6">
+        <div className="flex">
+          <Link href={"/"}>
+            <Image src={Logo} alt="Логотип" width={34} height={70} />
+          </Link>
+          <div>
+            <p>PRINT STUDIO</p>
+            <span>Москва • «тонкие армянские ноты»</span>
+          </div>
         </div>
+        <nav className="h-10">
+          <ul className="flex h-full items-center gap-6">
+            {navigation.map((e) => {
+              const isActiveLink = urlParam === e.href;
+              return (
+                <li key={e.href} className="h-full">
+                  <Link
+                    className={`${isActiveLink ? "nav-link-active" : ""} flex h-full w-full items-center justify-center p-2`}
+                    href={e.href}
+                  >
+                    {e.label}
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+        <Button>Оставить заявку</Button>
       </div>
-      <nav>
-        <ul>
-          {navigation.map((e, i) => {
-            const isActiveLink = urlParam === e.href;
-            return (
-              <li key={e.href}>
-                <Link
-                  className={isActiveLink ? "nav-link-active" : ""}
-                  href={e.href}
-                >
-                  {e.label}
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-      <Button>Оставить заявку</Button>
     </header>
   );
 }
