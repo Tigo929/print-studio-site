2071d8f8ce74c7e62dccec71891862f66573b3da
Gulyan Tigran <gulian2014@rambler.ru>
2026-01-06 15:54:23 +0300

    init: next.js + tailwind + shadcn

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import { link } from "fs";
+export default function Home() {
+  return (
+    <div className=" ">
+      <main>
+        <h1 className="text-4xl font-bold text-pink-700">Tailwind работает</h1>
+      </main>
+    </div>
+  );
+}
